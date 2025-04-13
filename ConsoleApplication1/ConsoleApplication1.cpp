33d7b1460b716efa2000de52d6f75339d7b3ee4a
Kyrylo <kirlingg@gmail.com>
2025-04-13 22:40:40 +0200

    ff

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,10 +1,16 @@
 ﻿
 #include <iostream>
+#include "math.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int add(int a, int b) {
+    return a + b;
 }
+
+int main() {
+    std::cout << "2 + 2 = " << add(2, 2) << std::endl;
+    return 0;
+}
+
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
